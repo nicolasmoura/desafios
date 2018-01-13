17c49209d0ec8761048c5a0ea7d7ded75817db57
nicolasmoura <nicolas-md@hotmail.com>
2018-01-13 14:17:47 -0200

    Finalizando parte 2 do primeiro desafio

--- strings/stringbreaker/src/main/java/org/nicolas/stringbreaker/service/StringService.java
+++ strings/stringbreaker/src/main/java/org/nicolas/stringbreaker/service/StringService.java
@@ -1,5 +1,6 @@
 package org.nicolas.stringbreaker.service;
 
+import org.nicolas.stringbreaker.Line;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -9,27 +10,52 @@ import java.util.List;
 public class StringService {
     public String breakLines(String text, int maxLineLength) {
         List<String> words = Arrays.asList(text.split("\\s"));
-        StringBuilder line = new StringBuilder(maxLineLength);
+        Line line = new Line();
         StringBuilder output = new StringBuilder();
 
         for (String word : words) {
             if (representsDoubleLinefeedCharacter(word)) {
                 word = "\n\n";
-                output.append(line.toString().trim()).append(word);
-                line = new StringBuilder(maxLineLength);
+                line = justify(line, maxLineLength);
+                output.append(line.getLineAsString().trim()).append(word);
+                line = new Line();
                 continue;
             }
-
-            if (line.length() + word.length() <= maxLineLength) {
-                line.append(word).append(" ");
+            if (line.getLength() + word.length() <= maxLineLength) {
+                line.getWords().add(word + " ");
+                line.addLength(word.length() + 1);
             } else {
-                output.append(line.toString().trim()).append("\n");
-                line = new StringBuilder(maxLineLength).append(word).append(" ");
+                line = justify(line, maxLineLength);
+                output.append(line.getLineAsString().trim()).append("\n");
+                line = new Line();
+                line.getWords().add(word + " ");
+                line.addLength(word.length() + 1);
             }
         }
-        output.append(line.toString());
+        line = justify(line, maxLineLength);
+        output.append(line.getLineAsString().trim());
 
         return output.toString().trim();
+    }
+
+    private Line justify(Line line, int maxLineLength) {
+        Line justifiedLine = new Line();
+        justifiedLine.setLength(line.getLength());
+        justifiedLine.setWords(line.getWords());
+
+        int numberOfSpacesToAdd = maxLineLength - justifiedLine.getLength() + 1;
+        int wordIndex = 0;
+        while (numberOfSpacesToAdd > 0) {
+            justifiedLine.getWords().set(wordIndex, getWordWithAddedSpace(justifiedLine, wordIndex));
+            wordIndex = wordIndex == justifiedLine.getWords().size() - 2 ? 0 : ++wordIndex;
+            numberOfSpacesToAdd--;
+        }
+
+        return justifiedLine;
+    }
+
+    private String getWordWithAddedSpace(Line linha, int wordIndex) {
+        return linha.getWords().get(wordIndex) + " ";
     }
 
     private boolean representsDoubleLinefeedCharacter(String value) {
