1293d8fd95fb08d8fd99ac5481d01c729b1ed8af
nicolasmoura <nicolas-md@hotmail.com>
2018-01-17 09:07:21 -0200

    organizando projetos

--- strings/stringbreaker-api/src/main/java/org/nicolas/stringbreaker/util/WordUtil.java
+++
@@ -1,9 +0,0 @@
-package org.nicolas.stringbreaker.util;
-
-import org.nicolas.stringbreaker.model.Line;
-
-public class WordUtil {
-    public static String getWordWithAddedSpace(Line linha, int wordIndex) {
-        return linha.getWords().get(wordIndex) + " ";
-    }
-}
